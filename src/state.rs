1d6180b6df73c10bfa8b029b64d72d65592e6fe5
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-03-16 23:48:54 -0700

    added insert functionality

--- src/state.rs
+++ src/state.rs
@@ -4,10 +4,7 @@ use std::hash::Hasher;
 use bytes;
 use linked_hash_map;
 
-use crate::global::Time;
-
-const DEFAULT_STATE_SHARD_CAPACITY: usize = 20;
-const MAX_STATE_SHARD_CAPACITY: usize = 100;
+use crate::utils::Encryption;
 
 type StateData = std::sync::Arc<
     Vec<
@@ -17,29 +14,21 @@ type StateData = std::sync::Arc<
     >,
 >;
 
-struct Status {
-    expires: Option<Time>,
-}
-
 struct State {
     current: StateData,
-    status: Status,
 }
 impl State {
-    pub fn get(self, key: &str) -> Option<bytes::Bytes> {
-        let mut hasher = std::collections::hash_map::DefaultHasher::new();
-        std::ptr::hash(key, &mut hasher);
+    pub fn get(self, key: String) -> Option<bytes::Bytes> {
+        let mut shard = self.get_shard(key.clone());
+        match shard.get(&key[..]) {
+            Some(val) => Some(val.lock().unwrap().clone()),
+            None => None,
+        }
+    }
 
-        let mut shard = self.current
-            [usize::try_from(hasher.finish()).unwrap() % self.current.len()]
-        .lock()
-        .unwrap();
-
-        let data = match shard.get(key).unwrap().lock().unwrap() {
-            val => Some(val.clone()),
-            _ => None,
-        };
-        data
+    pub fn insert(self, key: String, data: bytes::Bytes) {
+        let mut shard = self.get_shard(key.clone());
+        shard.insert(key, std::sync::Arc::new(std::sync::Mutex::new(data)));
     }
 
     pub fn new(capacity: usize) -> State {
@@ -51,7 +40,18 @@ impl State {
 
         State {
             current: std::sync::Arc::new(v),
-            status: Status { expires: None },
         }
+    }
+
+    fn get_shard(
+        &self,
+        key: String,
+    ) -> std::sync::MutexGuard<
+        linked_hash_map::LinkedHashMap<String, std::sync::Arc<std::sync::Mutex<bytes::Bytes>>>,
+    > {
+        let mut hash = Encryption::hash(key);
+        self.current[usize::try_from(hash).unwrap() % self.current.len()]
+            .lock()
+            .unwrap()
     }
 }
