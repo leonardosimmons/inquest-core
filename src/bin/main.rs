59cd75dc47c496e839a222b880316378189c70c2
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-03-19 11:12:16 -0700

    added StateResponse

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,19 @@
 #![allow(unused)]
 use inquest::state::{State, StateResponse};
 
+const DEFAULT_STATE_CAPACITY: usize = 1000;
+
 #[tokio::main]
 async fn main() {
-    let mut system_state = State::new(10);
+    let mut system_state = State::new(DEFAULT_STATE_CAPACITY);
 
     let state = system_state.clone();
 
-    state.insert("Hello", "World Test".into());
+    state.insert("Hello", "This is Test Number VI".into());
 
-    if let Some(val) = system_state.get("Hello") {
-        println!("get Value: {:?}", val);
-    } else {
-        println!("Key not found");
-    }
+    match state.get("Hello") {
+        StateResponse::Data(data) => println!("get Value: {:?}", data),
+        StateResponse::NotFound => println!("Key not found"),
+        _ => println!("Unexpected error has occurred"),
+    };
 }
--- src/state.rs
+++ src/state.rs
@@ -4,11 +4,15 @@ use std::hash::Hasher;
 use bytes;
 use linked_hash_map;
 
-use crate::utils::Encryption;
+use crate::utils::{Encryption, Error};
+
+const DEFAULT_STATE_ERROR_MSG: &str = "Unexpected error has occurred";
 
 pub enum StateResponse {
     Data(bytes::Bytes),
-    NoData,
+    Error(Error),
+    NotFound,
+    Ok,
 }
 
 type DataArray = linked_hash_map::LinkedHashMap<String, bytes::Bytes>;
@@ -19,19 +23,27 @@ pub struct State {
     state: DataMatrix,
 }
 impl State {
-    pub fn get(&self, key: &str) -> Option<bytes::Bytes> {
+    pub fn get(&self, key: &str) -> StateResponse {
         let mut shard = self.get_shard(key.clone());
         match shard.get(key) {
-            Some(val) => Some(val.clone()),
-            None => None,
+            Some(val) => StateResponse::Data(val.clone()),
+            None => StateResponse::NotFound,
+            _ => StateResponse::Error(Error {
+                msg: String::from(DEFAULT_STATE_ERROR_MSG),
+                timestamp: chrono::Utc::now(),
+            }),
         }
     }
 
-    pub fn insert(&self, key: &str, data: bytes::Bytes) -> Option<bytes::Bytes> {
+    pub fn insert(&self, key: &str, data: bytes::Bytes) -> StateResponse {
         let mut shard = self.get_shard(key.clone());
         match shard.insert(key.to_string(), data.clone()) {
-            Some(val) => Some(val),
-            None => None,
+            Some(val) => StateResponse::Data(val),
+            None => StateResponse::Ok,
+            _ => StateResponse::Error(Error {
+                msg: String::from(DEFAULT_STATE_ERROR_MSG),
+                timestamp: chrono::Utc::now(),
+            }),
         }
     }
 
@@ -47,21 +59,30 @@ impl State {
         }
     }
 
-    pub fn remove(&self, key: &str) -> Option<bytes::Bytes> {
+    pub fn remove(&self, key: &str) -> StateResponse {
         let mut shard = self.get_shard(key.clone());
         match shard.remove(key) {
-            Some(val) => Some(val),
-            None => None
+            Some(val) => StateResponse::Data(val),
+            None => StateResponse::NotFound,
+            _ => StateResponse::Error(Error {
+                msg: String::from("key not found"),
+                timestamp: chrono::Utc::now(),
+            }),
         }
     }
 
-    fn get_shard(
-        &self,
-        key: &str,
-    ) -> std::sync::MutexGuard<DataArray> {
-        let mut hash = Encryption::hash(key);
+    fn get_shard(&self, key: &str) -> std::sync::MutexGuard<DataArray> {
+        let mut hash = State::hash(key);
         self.state[usize::try_from(hash).unwrap() % self.state.len()]
             .lock()
             .unwrap()
+    }
+}
+
+impl Encryption for State {
+    fn hash(key: &str) -> u64 {
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        std::ptr::hash(key, &mut hasher);
+        hasher.finish()
     }
 }
