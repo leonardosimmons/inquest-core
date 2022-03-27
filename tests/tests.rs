72e534d0fbabb9b50da6c60c6de451076a001ce8
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-03-27 11:48:56 -0700

    updated

--- tests/tests.rs
+++ tests/tests.rs
@@ -8,32 +8,44 @@ mod parse {
         use tokio::io::AsyncReadExt;
 
         let mut buffer = String::new();
-        let mut file = File::open("./temp/test.html").await.unwrap();
+        let mut file = File::open("tests/test.html").await.unwrap();
         file.read_to_string(&mut buffer).await.unwrap();
         buffer
     }
 
     #[tokio::test]
-    async fn all_links_check() {
-        let file = test_file().await;
-        let html = Html::new(file);
-        let parse = Parse::new(html);
-        let links = parse.all_links().await.unwrap();
+    async fn get_headers() {
+        let h1_header = match Parse::new(Html::new(test_file().await))
+            .headers(HtmlTag::H1)
+            .await
+            .unwrap()
+        {
+            Headers::H1(h) => h[0].clone(),
+            _ => String::from(""),
+        };
+
+        assert_eq!("Welcome To The Test HTML File", h1_header);
+    }
+
+    #[tokio::test]
+    async fn get_links() {
+        let links = Parse::new(Html::new(test_file().await))
+            .all_links()
+            .await
+            .unwrap();
 
         assert_eq!(vec!["h2-link", "psub-a1-link", "psub-a2-link"], links);
     }
 
     #[tokio::test]
-    async fn get_title_tags() {
-        let file = test_file().await;
-        let html = Html::new(file);
-        let parse = Parse::new(html);
+    async fn get_title() {
+        let title = Parse::new(Html::new(test_file().await))
+            .page_title()
+            .await
+            .unwrap()[0]
+            .clone();
 
-        let h1_header = match parse.headers(HtmlTag::H1).await.unwrap() {
-            Headers::H1(h) => h[0].clone(),
-            _ => String::from(""),
-        };
-        assert_eq!("Welcome To The Test HTML File", h1_header);
+        assert_eq!("Test Html", title);
     }
 }
 
